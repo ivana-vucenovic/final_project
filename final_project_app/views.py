e6ce1512cfe677caa1d7c0769bf9e3732fed8238
Ivana Vucenovic <ivucenovic@hotmail.com>
2021-12-15 22:54:35 -0500

    9

--- final_project_app/views.py
+++ final_project_app/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, redirect
-# from django.contrib import messages
+from django.contrib import messages
 from .models import *
 import bcrypt
 
 def index(request):
     return render(request, 'main.html')
 
+# def register(request):
+#     return render(request, 'register.html')
+
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        errors=User.objects.registration_validator(request.POST)
+        if len(errors) > 0:
+            for key,value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt(rounds = 10)).decode()
+        new_user = User.objects.create(
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email=request.POST['email'],
+            password=pw_hash,
+        )
+        request.session['user_id'] = new_user.id
+        return redirect('/tables')
+    return redirect('/')
+
 
 def login(request):
     return render(request, 'login.html')
